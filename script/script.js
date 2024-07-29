d6a58a1a1974c0bd19e8b0e0b68eb0d5d5b00894
Dierovita <drochavitali@gmail.com>
2024-07-29 20:01:05 -0300

    V 0.4 - Finalizado Introdução

--- script/script.js
+++ script/script.js
@@ -26,28 +26,86 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', ajustarLink);
 })
 
-//////// SLIDER DE APRESENTAÇÃO JOGADORES ////////
+//////// SLIDER DE APRESENTAÇÃO JOGADORES - MOBILE////////
 document.addEventListener('DOMContentLoaded', function() {
-const slides = document.querySelectorAll('.players')
-const btn_next = document.querySelector('.next')
-const btn_prev = document.querySelector('.prev')
+const slides = document.querySelectorAll('#jogadores .players')
+const btn_next = document.querySelector('#jogadores .next')
+const btn_prev = document.querySelector('#jogadores .prev')
+const guias = document.querySelectorAll('#jogadores .slidGuia p')
+console.log(slides.length)
+
 let slideIndex = 0;
 
 btn_next.addEventListener('click', function() {
     slides[slideIndex].classList.remove('displayed');
-    slideIndex = (slideIndex + 1) % (slides.length -1);
+    guias[slideIndex].classList.remove('guiaAtual');
+    if (slideIndex == 4){
+    slideIndex = 0;
     slides[slideIndex].classList.add('displayed');
+    guias[slideIndex].classList.add('guiaAtual'); 
+    }
+    else{
+    slideIndex = (slideIndex + 1);
+    slides[slideIndex].classList.add('displayed');
+    guias[slideIndex].classList.add('guiaAtual');    
+    }
 })
 
 btn_prev.addEventListener('click', function() {
     slides[slideIndex].classList.remove('displayed');
+    guias[slideIndex].classList.remove('guiaAtual');
     if (slideIndex == 0){
         slideIndex = 4;
         slides[slideIndex].classList.add('displayed');
+        guias[slideIndex].classList.add('guiaAtual');
     }
     else{
     slideIndex = (slideIndex - 1) % (slides.length -1);
     slides[slideIndex].classList.add('displayed');
+    guias[slideIndex].classList.add('guiaAtual');
     }
 })
 })
+
+//////// SLIDER DE APRESENTAÇÃO JOGADORES - MOBILE////////
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('#jogadorespc .players')
+    const btn_next = document.querySelector('#jogadorespc .next')
+    const btn_prev = document.querySelector('#jogadorespc .prev')
+    const guias = document.querySelectorAll('#jogadorespc .slidGuia p')
+    console.log(slides.length)
+    
+    let slideIndex = 0;
+    
+    btn_next.addEventListener('click', function() {
+        slides[slideIndex].classList.remove('displayed');
+        guias[slideIndex].classList.remove('guiaAtual');
+        if (slideIndex == 4){
+        slideIndex = 0;
+        slides[slideIndex].classList.add('displayed');
+        guias[slideIndex].classList.add('guiaAtual'); 
+        }
+        else{
+        slideIndex = (slideIndex + 1);
+        slides[slideIndex].classList.add('displayed');
+        guias[slideIndex].classList.add('guiaAtual');    
+        }
+    })
+    
+    btn_prev.addEventListener('click', function() {
+        slides[slideIndex].classList.remove('displayed');
+        guias[slideIndex].classList.remove('guiaAtual');
+        if (slideIndex == 0){
+            slideIndex = 4;
+            slides[slideIndex].classList.add('displayed');
+            guias[slideIndex].classList.add('guiaAtual');
+        }
+        else{
+        slideIndex = (slideIndex - 1) % (slides.length -1);
+        slides[slideIndex].classList.add('displayed');
+        guias[slideIndex].classList.add('guiaAtual');
+        }
+    })
+    })
+
+
